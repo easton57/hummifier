5dd38f37e5a56a9dfa261dac380d570c8e14de4d
Easton Seidel <eseidel1357@gmail.com>
2024-02-16 12:11:10 -0800

    Some testing

--- main.py
+++ main.py
@@ -10,8 +10,12 @@ import media_reader as mr
 
 def main():
     """ Main Function """
-    # Call to read images
-    ml_data = mr.read_images('photos')
+    # Train
+    _, _, model = humming_ml.train('train_photos')
+
+    humming_ml.save_model(model)
+
+    humming_ml.inference(model, 'validate_photos/006.jpg')
 
 
 def monitor_video():
