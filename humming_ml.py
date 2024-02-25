7f1fd98042e99e0ffdbb9ad796789e7295425081
Easton Seidel <eseidel1357@gmail.com>
2024-02-24 17:59:55 -0800

    Converted to a HummingMLPhoto class

--- humming_ml.py
+++ humming_ml.py
@@ -5,7 +5,6 @@ import pathlib
 
 import matplotlib.pyplot as plt
 import numpy as np
-import PIL
 import tensorflow as tf
 
 from tensorflow import keras
@@ -13,140 +12,150 @@ from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
 
 
-def train_photo(path, plot=False) -> [Sequential, list]:
-    """ Method to train the hummingbird model """
-    # Some default parameters
-    batch_size = 32
-    img_height = 180
-    img_width = 180
+class HummingMLPhoto:
+    def __init__(self, model_name, load=False):
+        self.model_name = model_name
+        self.model = None
+        self.class_names = None
 
-    # Call to read images
-    path = pathlib.Path(path).with_suffix('')
+        if load:
+            self.load_model()
 
-    # training dataset
-    train_ds = tf.keras.utils.image_dataset_from_directory(
-        path,
-        validation_split=0.2,
-        subset="training",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
+    def train_photo(self, path, plot=False):
+        """ Method to train the hummingbird model """
+        # Some default parameters
+        batch_size = 32
+        img_height = 180
+        img_width = 180
 
-    # Validation dataset
-    val_ds = tf.keras.utils.image_dataset_from_directory(
-        path,
-        validation_split=0.2,
-        subset="validation",
-        seed=123,
-        image_size=(img_height, img_width),
-        batch_size=batch_size)
+        # Call to read images
+        path = pathlib.Path(path).with_suffix('')
 
-    # The category names
-    class_names = train_ds.class_names
+        # training dataset
+        train_ds = tf.keras.utils.image_dataset_from_directory(
+            path,
+            validation_split=0.2,
+            subset="training",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size)
 
-    # Set the data to stay in memory
-    AUTOTUNE = tf.data.AUTOTUNE
+        # Validation dataset
+        val_ds = tf.keras.utils.image_dataset_from_directory(
+            path,
+            validation_split=0.2,
+            subset="validation",
+            seed=123,
+            image_size=(img_height, img_width),
+            batch_size=batch_size)
 
-    train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
-    val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
+        # The category names
+        class_names = train_ds.class_names
 
-    # Image augmentation
-    data_augmentation = keras.Sequential(
-        [
-            layers.RandomFlip("horizontal",
-                              input_shape=(img_height,
-                                           img_width,
-                                           3)),
-            layers.RandomRotation(0.1),
-            layers.RandomZoom(0.1),
-        ]
-    )
+        # Set the data to stay in memory
+        AUTOTUNE = tf.data.AUTOTUNE
 
-    # Define our model
-    num_classes = len(class_names)
+        train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=AUTOTUNE)
+        val_ds = val_ds.cache().prefetch(buffer_size=AUTOTUNE)
 
-    model = Sequential([
-        data_augmentation,
-        layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),  # Standardization layer
-        layers.Conv2D(16, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(32, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Conv2D(64, 3, padding='same', activation='relu'),
-        layers.MaxPooling2D(),
-        layers.Flatten(),
-        layers.Dense(128, activation='relu'),
-        layers.Dense(num_classes)
-    ])
+        # Image augmentation
+        data_augmentation = keras.Sequential(
+            [
+                layers.RandomFlip("horizontal",
+                                  input_shape=(img_height,
+                                               img_width,
+                                               3)),
+                layers.RandomRotation(0.1),
+                layers.RandomZoom(0.1),
+            ]
+        )
 
-    model.compile(optimizer='adam',
-                  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
+        # Define our model
+        num_classes = len(class_names)
 
-    # Train the model
-    epochs = 10
-    history = model.fit(
-        train_ds,
-        validation_data=val_ds,
-        epochs=epochs
-    )
+        model = Sequential([
+            data_augmentation,
+            layers.Rescaling(1. / 255, input_shape=(img_height, img_width, 3)),  # Standardization layer
+            layers.Conv2D(16, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(32, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Conv2D(64, 3, padding='same', activation='relu'),
+            layers.MaxPooling2D(),
+            layers.Flatten(),
+            layers.Dense(128, activation='relu'),
+            layers.Dense(num_classes)
+        ])
 
-    # Show the results if wanted
-    if plot:
-        acc = history.history['accuracy']
-        val_acc = history.history['val_accuracy']
+        model.compile(optimizer='adam',
+                      loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                      metrics=['accuracy'])
 
-        loss = history.history['loss']
-        val_loss = history.history['val_loss']
+        # Train the model
+        epochs = 10
+        history = model.fit(
+            train_ds,
+            validation_data=val_ds,
+            epochs=epochs
+        )
 
-        epochs_range = range(epochs)
+        # Show the results if wanted
+        if plot:
+            acc = history.history['accuracy']
+            val_acc = history.history['val_accuracy']
 
-        plt.figure(figsize=(8, 8))
-        plt.subplot(1, 2, 1)
-        plt.plot(epochs_range, acc, label='Training Accuracy')
-        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-        plt.legend(loc='lower right')
-        plt.title('Training and Validation Accuracy')
+            loss = history.history['loss']
+            val_loss = history.history['val_loss']
 
-        plt.subplot(1, 2, 2)
-        plt.plot(epochs_range, loss, label='Training Loss')
-        plt.plot(epochs_range, val_loss, label='Validation Loss')
-        plt.legend(loc='upper right')
-        plt.title('Training and Validation Loss')
-        plt.show()
+            epochs_range = range(epochs)
 
-    return model, class_names
+            plt.figure(figsize=(8, 8))
+            plt.subplot(1, 2, 1)
+            plt.plot(epochs_range, acc, label='Training Accuracy')
+            plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+            plt.legend(loc='lower right')
+            plt.title('Training and Validation Accuracy')
 
+            plt.subplot(1, 2, 2)
+            plt.plot(epochs_range, loss, label='Training Loss')
+            plt.plot(epochs_range, val_loss, label='Validation Loss')
+            plt.legend(loc='upper right')
+            plt.title('Training and Validation Loss')
+            plt.show()
 
-def infer_photo(model, image_path, class_names) -> None:
-    """ Method to evaluate an image or video stream """
-    img_height = 180
-    img_width = 180
+        self.model = model
+        self.class_names = class_names
 
-    img = tf.keras.utils.load_img(
-        image_path, target_size=(img_height, img_width)
-    )
-    img_array = tf.keras.utils.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create a batch
+    def infer_photo(self, image_path) -> None:
+        """ Method to evaluate an image or video stream """
+        if self.model is None:
+            print("No model has been loaded! Train one or load one from the models directory...")
+            return
 
-    predictions = model.predict(img_array)
-    score = tf.nn.softmax(predictions[0])
+        img_height = 180
+        img_width = 180
 
-    print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-    )
+        img = tf.keras.utils.load_img(
+            image_path, target_size=(img_height, img_width)
+        )
+        img_array = tf.keras.utils.img_to_array(img)
+        img_array = tf.expand_dims(img_array, 0)  # Create a batch
 
+        predictions = self.model.predict(img_array)
+        score = tf.nn.softmax(predictions[0])
 
-def image_clustering():
-    """ Method to cluster images to verify the effectiveness of the standardization """
+        print(
+            "This image most likely belongs to {} with a {:.2f} percent confidence."
+            .format(self.class_names[np.argmax(score)], 100 * np.max(score))
+        )
 
+    def image_clustering(self, ):
+        """ Method to cluster images to verify the effectiveness of the standardization """
 
-def save_model(model) -> None:
-    """ Method to save the trained model """
-    model.save('models/hummifier.pkl')
+    def save_model(self, model) -> None:
+        """ Method to save the trained model """
+        model.save(f'models/{self.model_name}.pkl')
 
-
-def load_model() -> Sequential:
-    """ Method to load the model from a file """
-    return tf.keras.models.load_model('models/hummifier.pkl')
+    def load_model(self, ):
+        """ Method to load the model from a file """
+        self.model = tf.keras.models.load_model(f'models/{self.model_name}.pkl')
