9db7067696181fc9c6c71dd36473bad85945c98a
Easton Seidel <eseidel1357@gmail.com>
2024-02-25 00:28:21 -0800

    Used to split video files into 15 second increments for manual sorting training

---
+++ video_split.py
@@ -0,0 +1,26 @@
+""" File for splitting large video files into smaller trainable videos """
+from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
+
+
+def split_video(file_name, increment, vid_num):
+    # Get our length in seconds
+    video_length = VideoFileClip(file_name).duration
+    curr_time = 0
+    times = []
+
+    while curr_time < video_length:
+        if curr_time + increment > video_length:
+            times.append([curr_time, video_length])
+        else:
+            times.append([curr_time, curr_time + increment])
+
+        curr_time += increment
+
+    for time in times:
+        start_time = int(time[0])
+        end_time = int(time[1])
+        ffmpeg_extract_subclip(file_name,
+                               start_time,
+                               end_time,
+                               targetname=f"train_video/outside_window_{vid_num}_{time[0]}-{time[1]}.mp4")
